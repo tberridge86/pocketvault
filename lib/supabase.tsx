98f45ecd493bddcd226d497a96fae390073c38c8
Tom Berridge <tberridge86@gmail.com>
2026-04-28 15:53:57 +0100

    apk changes

--- lib/supabase.tsx
+++ lib/supabase.tsx
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = 'https://oakdbbzdqwurpjnoqhmu.supabase.co';
+const supabaseAnonKey = 'sb_publishable_utiXk-8YPG57MWlrYdWgvg_7xaufYYt';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
